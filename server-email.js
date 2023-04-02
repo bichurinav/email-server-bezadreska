12f13940707e7289cefcedc4eaa4e8e727bab620
Artem Bichurin <bichurin.overclock1ng@gmail.com>
2023-04-02 17:17:54 +0500

    init

---
+++ server-email.js
@@ -0,0 +1,59 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://127.0.0.1:5500",
+      "https://bezadreska.ru",
+      "http://localhost",
+    ],
+  })
+);
+
+const { login, password, email } = dotenv.config().parsed;
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.yandex.ru",
+  port: 465,
+  secure: true,
+  auth: {
+    user: login,
+    pass: password,
+  },
+});
+
+app.use(express.json());
+
+app.post("/feedback", (req, res) => {
+  const { name, number, message } = req.body;
+  const date = new Date();
+  const currentMonth = date.getUTCMonth() + 1;
+  const parsedMonth =
+    currentMonth.toString().length > 1
+      ? currentMonth
+      : `0${currentMonth.toString()}`;
+  const parsedFullDate = `${date.getUTCDate()}.${parsedMonth}.${date.getUTCFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+  const mailOptions = {
+    from: email,
+    to: email,
+    subject: `Заявка от клиента: ${name}`,
+    text: `Имя клиента: ${name}. \nТелефон: ${number}.\n Комментарий: ${
+      message.length > 0 ? message : "отсутствует"
+    }\n Дата: ${parsedFullDate}`,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.status(500).json({ status: "error" });
+    } else {
+      res.status(200).json({ status: "successful" });
+    }
+  });
+});
+
+app.listen(3003, () => {
+  console.log("Server is started!");
+});
